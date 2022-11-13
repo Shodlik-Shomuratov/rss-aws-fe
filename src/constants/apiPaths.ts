1ac62075460bc7926a26837e1d746340937d6cba
Shodlik Shomuratov <shodlikcoder@gmail.com>
2022-11-13 15:18:02 +0500

    Initial commit

---
+++ src/constants/apiPaths.ts
@@ -0,0 +1,9 @@
+const API_PATHS = {
+  product: "https://354lhj9ule.execute-api.ap-southeast-1.amazonaws.com/dev/products",
+  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://develop.ap-southeast-1.elasticbeanstalk.com/api",
+};
+
+export default API_PATHS;
